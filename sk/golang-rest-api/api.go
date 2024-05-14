8e02b282b10614926c8f6035f70de04c6ef52ac6
Huynh Huu Khang <huukhang1999@gmail.com>
2024-05-14 21:35:30 +0700

    doing rest api gorrila http

---
+++ sk/golang-rest-api/api.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type APIServer struct {
+	addr  string
+	store Store
+}
+
+func NewAPIServer(addr string, store Store) *APIServer {
+	return &APIServer{
+		addr:  addr,
+		store: store,
+	}
+}
+
+func (s *APIServer) Serve() {
+	// Start the server
+	router := mux.NewRouter()
+	subRouter := router.PathPrefix("/api/v1").Subrouter()
+
+	// registering our services
+	log.Println("Starting the API server at", s.addr)
+
+	log.Fatal(http.ListenAndServe(s.addr, subRouter))
+
+}
