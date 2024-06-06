468ba04ef34e5a3f6672897e2083b5b26ed9527a
Huynh Huu Khang <khang.huynh@amoeba.group>
2024-06-06 11:28:45 +0700

    handle pagination get list

--- sk/golang-scalable-backend/main.go
+++ sk/golang-scalable-backend/main.go
@@ -4,13 +4,15 @@ import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 )
 
 type TodoItem struct {
-	Id          int        `json:"id" gorm:"column:id,primaryKey"`
+	Id          int        `json:"id" gorm:"column:id"`
 	Title       string     `json:"title" gorm:"column:title"`
 	Description string     `json:"description" gorm:"column:description"`
 	Status      string     `json:"status" gorm:"column:status"`
@@ -18,21 +20,47 @@ type TodoItem struct {
 	UpdatedAt   *time.Time `json:"updated_at,omitempty" gorm:"updated_at"` // neu co pointer se lay gia tri null insert vao db
 }
 
+type Pagination struct {
+	Page  int `json:"page" form:"page"`
+	Limit int `json:"limit" form:"limit"`
+}
+
+func (p *Pagination) Process() {
+	if p.Page < 1 {
+		p.Page = 1
+	}
+
+	if p.Limit < 1 || p.Limit > 50 {
+		p.Limit = 12
+	}
+}
+
 type TodoItemCreate struct {
-	Id          int    `json:"id" gorm:"column:id,primaryKey"`
+	Id          int    `json:"id" gorm:"column:id"`
 	Title       string `json:"title" gorm:"column:title"`
 	Description string `json:"description" gorm:"column:description"`
 	Status      string `json:"status" gorm:"column:status"`
 }
+type TodoItemUpdate struct {
+	Title       *string `json:"title" gorm:"column:title"` //pointer cho phep update chuoi rong
+	Description *string `json:"description" gorm:"column:description"`
+	Status      *string `json:"status" gorm:"column:status"`
+}
 
 func (TodoItem) TableName() string       { return "todo_items" }
 func (TodoItemCreate) TableName() string { return TodoItem{}.TableName() }
+func (TodoItemUpdate) TableName() string { return TodoItem{}.TableName() }
 
 func main() {
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	//root:my-secret-pw@tcp(127.0.0.1:3309)/social-todolist?charset=utf8mb4&parseTime=True&loc=Local
-	dsn := "root@tcp(localhost:3306/social-todolist?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	dsn := "root:my-secret-pw@tcp(127.0.0.1:3309)/social-todolist?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info), // Log SQL
+	})
+
+	//db = db.Debug() // Debug SQL
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -45,10 +73,10 @@ func main() {
 		items := v1.Group("/items")
 		{
 			items.GET("", GetListItem(db)) // phai call () vi function nay return ve 1 function khac ( closure )
-			items.GET("/:id")
+			items.GET("/:id", GetItem(db))
 			items.POST("", CreateItem(db))
-			items.PUT("/:id")
-			items.DELETE("/:id")
+			items.PUT("/:id", UpdateItem(db))
+			items.DELETE("/:id", DeleteItem(db))
 		}
 	}
 	r.GET("/ping", func(c *gin.Context) {
@@ -60,28 +88,6 @@ func main() {
 }
 
 func CreateItem(db *gorm.DB) func(ctx *gin.Context) {
-	return func(ctx *gin.Context) {
-		itemData := TodoItem{}
-		if err := ctx.ShouldBind(&itemData); err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-
-		if err := db.Create(&itemData).Error; err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{
-				"error": err.Error(),
-			})
-		}
-
-		ctx.JSON(http.StatusOK, gin.H{
-			"data": itemData.Id,
-		})
-	}
-}
-
-func GetListItem(db *gorm.DB) func(ctx *gin.Context) {
 	return func(ctx *gin.Context) {
 		itemData := TodoItemCreate{}
 		if err := ctx.ShouldBind(&itemData); err != nil {
@@ -99,6 +105,116 @@ func GetListItem(db *gorm.DB) func(ctx *gin.Context) {
 
 		ctx.JSON(http.StatusOK, gin.H{
 			"data": itemData.Id,
+		})
+	}
+}
+
+func GetItem(db *gorm.DB) func(ctx *gin.Context) {
+	return func(ctx *gin.Context) {
+		var itemData TodoItemCreate
+		id, err := strconv.Atoi(ctx.Param("id"))
+
+		log.Println("id", id)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if err := db.Where("id = ?", id).First(&itemData).Error; err != nil { // First lay 1 row, 1 hoac nhieu thi lay Find
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"data": itemData,
+		})
+	}
+}
+
+func UpdateItem(db *gorm.DB) func(ctx *gin.Context) {
+	return func(ctx *gin.Context) {
+
+		id, err := strconv.Atoi(ctx.Param("id"))
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		updateData := TodoItemUpdate{}
+
+		if err := ctx.ShouldBind(&updateData); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if err := db.Where("id = ?", id).Updates(updateData).Error; err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"data": true,
+		})
+	}
+}
+
+func DeleteItem(db *gorm.DB) func(ctx *gin.Context) {
+	return func(ctx *gin.Context) {
+		id, err := strconv.Atoi(ctx.Param("id"))
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		deleteStatus := "Deleted"
+		if err := db.Where("id = ?", id).Updates(&TodoItemUpdate{Status: &deleteStatus}).Error; err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"data": true,
+		})
+	}
+}
+
+func GetListItem(db *gorm.DB) func(ctx *gin.Context) {
+	return func(ctx *gin.Context) {
+		var pagination Pagination
+
+		if err := ctx.ShouldBindQuery(&pagination); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		pagination.Process()
+
+		var items []TodoItem
+
+		if err := db.Find(&items).Error; err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"data": items,
 		})
 	}
 }
